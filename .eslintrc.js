8b8b92eac097a95dad3e4818656565bcfed21a9a
vijaybaliah <vijaybalu1989@gmail.com>
2023-01-30 00:30:25 +0530

    style(lint): added sonar lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,20 +7,43 @@ module.exports = {
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'airbnb-typescript',
+        'plugin:sonarjs/recommended',
         'prettier'
     ],
     'overrides': [
     ],
     'parser': '@typescript-eslint/parser',
     'parserOptions': {
+        'project': './tsconfig.json',
         'ecmaVersion': 'latest',
         'sourceType': 'module'
     },
+    'settings': {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
+        },
+        'import/resolver': {
+            'typescript': {
+                'alwaysTryTypes': true,
+                'paths': './tsconfig.json'
+            },
+            'node': {
+                'extensions': ['.ts', '.tsx', '.js', '.jsx']
+            }
+        }
+    },
     'plugins': [
         'react',
-        '@typescript-eslint'
+        '@typescript-eslint',
+        'sonarjs'
     ],
     'rules': {
+        'react/react-in-jsx-scope': 'off',
         'linebreak-style': [
             'error',
             'unix'
